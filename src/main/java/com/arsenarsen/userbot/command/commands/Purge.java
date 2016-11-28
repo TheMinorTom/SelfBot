6c8b2a1e3e729233fd41186268f5eac33efb7c89
ArsenArsen <ArsenArsen@users.noreply.github.com>
2016-11-28 19:47:43 +0100

    Fix purge

---
+++ src/main/java/com/arsenarsen/userbot/command/commands/Purge.java
@@ -0,0 +1,23 @@
+package com.arsenarsen.userbot.command.commands;
+
+import com.arsenarsen.userbot.UserBot;
+import com.arsenarsen.userbot.command.Command;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+
+public class Purge implements Command {
+    @Override
+    public void dispatch(String[] args, MessageChannel channel, Message msg) {
+        if (args.length == 1 && args[0].matches("\\d+")) {
+            channel.getHistory().retrievePast(Integer.parseInt(args[0])).queue(messages -> messages
+                    .stream()
+                    .filter(msg2 -> msg2.getAuthor().equals(UserBot.getInstance().getJda().getSelfUser()))
+                    .forEach(message -> message.deleteMessage().queue()));
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "purge";
+    }
+}
