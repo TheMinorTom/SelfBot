87b346d75f096815dac1ae806970351c0cb5ce9b
MinorTom <minortom@fantasymail.de>
2016-12-03 19:25:35 +0100

    License change

--- src/main/java/com/arsenarsen/userbot/websocket/WebSocketMessageHandler.java
+++ src/main/java/com/arsenarsen/userbot/websocket/WebSocketMessageHandler.java
@@ -1,5 +1,9 @@
 package com.arsenarsen.userbot.websocket;
 
+/*
+ *  This file is Copyright 2016 by MinorTom and licensed under the MIT License
+ */
+
 public interface WebSocketMessageHandler{
     WebSocketMessage dispatch(WebSocketMessage msg);
 
